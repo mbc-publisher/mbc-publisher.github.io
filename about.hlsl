b03a85cb0f174ed150c175190fa04fa9a2f10248
Mykaël Boyte-Coutu <mbc.publisher@gmail.com>
2026-02-09 20:25:41 -0500

    Add HTML structure for about page

---
+++ about.hlsl
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MBC Publisher</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <header>
+        <img src="assets/logo.png" alt="MBC" class="logoSmall">
+        <div class="logo-title-container">
+            MBC Publisher
+        </div>
+
+        <nav>
+            <ul>
+                <li><a href="#games">Project</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+
+        <section id="about">
+            <img src="assets/logo.png" alt="MBC" class="logo">
+
+            <h2>About Us</h2>
+            <p>Mykaël Boyte-Coutu, solo dev. Currently working as a tech artist at Ubisoft Montréal.</p>
+            <p>Pationate about non-physical rendering.</p>
+        </section>
+
+    </main>
+
+    <footer>
+        <p>&copy; MBC Publisher</p>
+    </footer>
+</body>
+
+</html>
